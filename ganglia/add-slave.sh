a95de36c12d2ad9bbe3ae7ec8876212326f8c299
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2014-04-23 16:29:38 -0700

    Set ganglia packages, copy conf in add-slave.sh

--- ganglia/add-slave.sh
+++ ganglia/add-slave.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
+
 for node in $ADD_SLAVES; do
   ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
 done
 
 wait
+
+/root/spark-ec2/copy-dir /etc/ganglia/
 
 for node in $ADD_SLAVES; do
   ssh -t -t $SSH_OPTS root@$node "/etc/init.d/gmond restart"

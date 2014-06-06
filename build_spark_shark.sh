bfd42b99268306f16b0d9c30af09bf312435cb3a
root <root@ip-10-232-9-223.ec2.internal>
2014-06-06 18:25:55 +0000

    Added file to build Spark/Shark from scratch.

---
+++ build_spark_shark.sh
@@ -0,0 +1,29 @@
+echo "Setting up Spark"
+mv spark spark_old
+git clone -b proc_logging_minimal https://github.com/kayousterhout/spark-1.git spark
+cp spark_old/conf/* spark/conf/
+pushd spark
+SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.2.0 sbt/sbt clean assembly publish-local
+popd
+
+echo "Setting up Hive"
+sudo yum install ant
+git clone https://github.com/amplab/hive.git -b shark-0.11
+pushd hive
+ant package
+popd
+
+echo "Setting up Shark"
+mv shark shark_old
+git clone -b branch-0.9 https://github.com/amplab/shark.git
+cp shark_old/conf/shark-env.sh spark/conf/
+pushd shark
+SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.2.0 sbt/sbt package
+popd
+
+echo "Copying files to cluster"
+spark-ec2/copy-dir --delete spark
+spark-ec2/copy-dir --delete shark
+spark-ec2/copy-dir --delete hive
+
+

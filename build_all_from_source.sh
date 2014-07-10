5e3495530f6d5d1277453b4004140cece0bade7d
root <root@ip-10-171-2-162.ec2.internal>
2014-07-10 20:42:57 +0000

    Don't build hive, use mvn absolute path.

--- build_all_from_source.sh
+++ build_all_from_source.sh
@@ -1,3 +1,7 @@
+# Exit if any commands fail.
+set -e
+set -o pipefail
+
 pushd /root
 
 echo "-------------------Installing Protobufs"
@@ -13,34 +17,23 @@ echo "-------------------Installing Maven"
 wget http://apache.spinellicreations.com/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
 tar -xvzf apache-maven-3.2.1-bin.tar.gz
 mv apache-maven-3.2.1 /usr/local/apache-maven/
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.2.1
-export M2=$M2_HOME/bin
-export PATH=$M2:$PATH
-echo "-------------------Verifying Maven install worked properly"
-mvn --version
 
 echo "-------------------Building Hadoop"
+git clone -b branch-2.0.2-withlogging https://github.com/kayousterhout/hadoop-common.git
 pushd hadoop-common
-mvn package -DskipTests -Pdist -Dmaven.javadoc.skip=true -e -X
+/usr/local/apache-maven/bin/mvn package -DskipTests -Pdist -Dmaven.javadoc.skip=true -e -X
 # mvn install puts Hadoop in the ~/.m2 directory, where Spark/Shark can find it.
-mvn install -DskipTests
+/usr/local/apache-maven/bin/mvn install -DskipTests
 popd
 
 echo "-------------------Setting up Spark"
 mv spark spark_old
-git clone -b proc_logging_minimal https://github.com/kayousterhout/spark-1.git spark
+git clone -b proc_logging_perf_minimal_temp https://github.com/kayousterhout/spark-1.git spark
 cp spark_old/conf/* spark/conf/
 pushd spark
 # Similar to mvn install, sbt/sbt publish-local puts Spark in the ~/.ivy2 directory,
 # where Shark can find it.
 SPARK_HADOOP_VERSION=2.0.2-kay sbt/sbt clean assembly publish-local
-popd
-
-echo "Setting up Hive"
-sudo yum install ant
-git clone https://github.com/amplab/hive.git -b shark-0.11
-pushd hive
-ant package
 popd
 
 echo "Setting up Shark"
@@ -51,12 +44,11 @@ pushd shark
 # Note that if you need to rebuild Shark after fixing something in Spark,
 # you need to do "sbt/sbt clean", otherwise sbt won't pick up the newer Spark
 # dependency in the local Ivy repository (in ~/.ivy2).
-SPARK_HADOOP_VERSION=2.0.2-kay sbt/sbt package
+SPARK_HADOOP_VERSION=2.0.2-kay sbt/sbt clean package
 popd
 
 echo "Copying files to cluster"
 spark-ec2/copy-dir --delete spark
 spark-ec2/copy-dir --delete shark
-spark-ec2/copy-dir --delete hive
 
 popd

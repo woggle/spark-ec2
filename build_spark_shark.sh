35bdd0779ee5004e31dec754d69073d4dd14c126
root <root@ip-10-6-39-21.ec2.internal>
2014-07-10 17:02:06 +0000

    Updated build script to also build Hadoop.
    In order to build Hadoop, need to install mvn and protobufs.

--- build_spark_shark.sh
+++ build_spark_shark.sh
@@ -1,11 +1,39 @@
 pushd /root
 
-echo "Setting up Spark"
+echo "-------------------Installing Protobufs"
+wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz
+tar -xvzf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0
+./configure
+make
+sudo make install
+sudo ldconfig
+
+echo "-------------------Installing Maven"
+wget http://apache.spinellicreations.com/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
+tar -xvzf apache-maven-3.2.1-bin.tar.gz
+mv apache-maven-3.2.1 /usr/local/apache-maven/
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.2.1
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+echo "-------------------Verifying Maven install worked properly"
+mvn --version
+
+echo "-------------------Building Hadoop"
+pushd hadoop-common
+mvn package -DskipTests -Pdist -Dmaven.javadoc.skip=true -e -X
+# mvn install puts Hadoop in the ~/.m2 directory, where Spark/Shark can find it.
+mvn install -DskipTests
+popd
+
+echo "-------------------Setting up Spark"
 mv spark spark_old
 git clone -b proc_logging_minimal https://github.com/kayousterhout/spark-1.git spark
 cp spark_old/conf/* spark/conf/
 pushd spark
-SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.2.0 sbt/sbt clean assembly publish-local
+# Similar to mvn install, sbt/sbt publish-local puts Spark in the ~/.ivy2 directory,
+# where Shark can find it.
+SPARK_HADOOP_VERSION=2.0.2-kay sbt/sbt clean assembly publish-local
 popd
 
 echo "Setting up Hive"
@@ -20,7 +48,10 @@ mv shark shark_old
 git clone -b branch-0.9 https://github.com/amplab/shark.git
 cp shark_old/conf/shark-env.sh shark/conf/
 pushd shark
-SPARK_HADOOP_VERSION=2.0.0-mr1-cdh4.2.0 sbt/sbt package
+# Note that if you need to rebuild Shark after fixing something in Spark,
+# you need to do "sbt/sbt clean", otherwise sbt won't pick up the newer Spark
+# dependency in the local Ivy repository (in ~/.ivy2).
+SPARK_HADOOP_VERSION=2.0.2-kay sbt/sbt package
 popd
 
 echo "Copying files to cluster"

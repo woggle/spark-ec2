05c7500a760a6109ffb0e537b828e5b146ca1e42
Kay Ousterhout <kayousterhout@gmail.com>
2014-07-11 23:15:56 +0000

    Modified default spark-env to turn on job logger

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -17,7 +17,7 @@ export SCALA_HOME="/root/scala"
 export SPARK_MEM={{default_spark_mem}}
 
 # Set JVM options and Spark Java properties
-SPARK_JAVA_OPTS+=" -Dspark.local.dir={{spark_local_dirs}}"
+SPARK_JAVA_OPTS+=" -Dspark.jobLogger.enabled=true -Dspark.local.dir={{spark_local_dirs}}"
 export SPARK_JAVA_OPTS
 
 export HADOOP_HOME="/root/ephemeral-hdfs"

c5d849087bfd5329ef42903c74aeac7a4a6344ca
Ryan Rasti <ryanrasti@gmail.com>
2014-07-16 16:17:08 -0700

    *fix log getter

--- run_all_output.sh
+++ run_all_output.sh
@@ -6,17 +6,14 @@ do
   OUTPUT_FILENAME="/tmp/query_out_$FILENAME"
   echo "Running query in file $query_file and sending output to $OUTPUT_FILENAME"
   # Clear buffer cache before each query!
+  python /root/shark/bin/dev/clear-buffer-cache.py
+  /root/shark/bin/shark-withinfo -h localhost -p 4444 -i $query_file > $OUTPUT_FILENAME 2>&1
 
   TIMER_FILE_RELATIVE=`ls /tmp/spark-root/ | sort | tail -1`
+  echo file is $TIMER_FILE_RELATIVE
   TIMER_FILE="/tmp/spark-root/$TIMER_FILE_RELATIVE"
 
   OUTPUT_FOLDER="/mnt/output/$FILENAME-$TIMER_FILE_RELATIVE"
-
-
-  
-
-  python /root/shark/bin/dev/clear-buffer-cache.py
-  /root/shark/bin/shark-withinfo -h localhost -p 4444 -i $query_file > $OUTPUT_FILENAME 2>&1
 
   cp -r $TIMER_FILE $OUTPUT_FOLDER
   cp $OUTPUT_FILENAME $OUTPUT_FOLDER

9fabe29f27967cdaf46670685e1e14d4870f6bab
Charles Reiss <woggle@apache.org>
2014-11-03 14:43:57 -0800

    Diceware passwords; support using existing certs

--- templates/root/cs194-16/machine_setup.sh
+++ templates/root/cs194-16/machine_setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+CERT_SUBJECT='/CN=CS194-16 Fall 2014 Lab 9/O=UC Berkeley/L=Berkeley/ST=CA/C=US'
+
 # Create certificate for the cluster (so we can connect w/ HTTPS and not
 # send cleartext password).
-pushd /root
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mycert.pem -out mycert.pem -batch
-popd
+if [ -f /root/cs194-16/mycert.pem ]; then
+    cp /root/cs194-16/mycert.pem /root/mycert.pem
+else
+    pushd /root
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mycert.pem -out mycert.pem -subj "$CERT_SUBJECT" -batch
+    popd
+fi
 
 # Kill all of the existing screens.
 screen -ls | grep Detached | cut -d . -f 1 | awk '{print $1}' | xargs kill
@@ -12,9 +18,14 @@ screen -ls | grep Detached | cut -d . -f 1 | awk '{print $1}' | xargs kill
 # Create a python profile to use.
 ipython profile create default
 
-PASSWD=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 8`
+PASSWD=`python27 -c '
+from random import SystemRandom
+lines = map(str.strip, open("diceware_list.txt", "r").readlines())
+print " ".join([SystemRandom().choice(lines) for i in range(3)])
+'`
 echo "********************************************************"
 echo `wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname` $PASSWD
+echo "$PASSWD" >clear_passwd.txt
 python -c "from IPython.lib import passwd; print passwd('$PASSWD')" > /root/.ipython/profile_default/nbpasswd.txt
 
 cp /root/cs194-16/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py

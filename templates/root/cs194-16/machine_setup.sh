7bdcc640d5bdaf0576c7ebed64048a0381e3c0d3
Charles Reiss <woggle@apache.org>
2014-11-04 13:58:18 -0800

    Full path to remove notebooks; avoid regenerating certs

--- templates/root/cs194-16/machine_setup.sh
+++ templates/root/cs194-16/machine_setup.sh
@@ -6,14 +6,14 @@ CERT_SUBJECT='/CN=CS194-16 Fall 2014 Lab 9/O=UC Berkeley/L=Berkeley/ST=CA/C=US'
 # send cleartext password).
 if [ -f /root/cs194-16/mycert.pem ]; then
     cp /root/cs194-16/mycert.pem /root/mycert.pem
-else
+elif [ ! -f /root/mycert.pem ]; then
     pushd /root
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mycert.pem -out mycert.pem -subj "$CERT_SUBJECT" -batch
     popd
 fi
 
 
-rm lab8*.ipynb
+rm /root/cs194-16/lab8*.ipynb
 
 # Kill all of the existing screens.
 screen -ls | grep Detached | cut -d . -f 1 | awk '{print $1}' | xargs kill
@@ -28,7 +28,7 @@ print " ".join([SystemRandom().choice(lines) for i in range(3)])
 '`
 echo "********************************************************"
 echo `wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname` $PASSWD
-echo "$PASSWD" >clear_passwd.txt
+echo "$PASSWD" >/root/clear_passwd.txt
 python -c "from IPython.lib import passwd; print passwd('$PASSWD')" > /root/.ipython/profile_default/nbpasswd.txt
 
 cp /root/cs194-16/ipython_notebook_config.py /root/.ipython/profile_default/ipython_notebook_config.py
